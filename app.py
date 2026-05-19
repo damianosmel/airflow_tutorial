9784c133fbb8510f8e70d559177f93a41c187541
damianosmel <damianosmel@gmail.com>
2026-05-19 17:35:56 +0300

    Add a new endpoint to trigger a dag Extra: add httpbasicauth object for the authentication process

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import json
 import requests
+from requests.auth import HTTPBasicAuth
 import os
 import time
 from typing import Optional
 
-AIRFLOW_API = os.getenv("AIFLOW_API","http://localhost:8080/api/v1")
+AIRFLOW_API = os.getenv("AIFLOW_API","http://localhost:8080/api/v2")
 DAG_ID = "taskflow_fastapi"
 CREDENTIALS_PATH = "/home/dmelidis/airflow/simple_auth_manager_passwords.json.generated"
-AIRFLOW_AUTH = None
 POLL_INTERVAL = 1.0
 POLL_TIMEOUT = 30.0
 
@@ -19,23 +20,35 @@ app = FastAPI()
 def _load_credentials() -> Optional[dict[str,str]]:
     with open(CREDENTIALS_PATH) as credentials_fp:
         credentials_data = json.load(credentials_fp)
-    
         if "admin" in credentials_data:
-            admin_creds = ("admin",credentials_data["admin"])
+            admin_creds = HTTPBasicAuth("admin",credentials_data["admin"])
         else:
             admin_creds = None
     return admin_creds
 
 class Payload(BaseModel):
-    __root__: dict
+    root: dict[str,float]
+
+@app.post("/trigger/{dag_run_id}")
+def trigger(dag_run_id: str, payload: dict):
+    airflow_auth = _load_credentials()
+    print(f'auth: {airflow_auth}')
+    resp = requests.post(
+        f"{AIRFLOW_API}/dags/{DAG_ID}/dagRuns",
+        json={"dag_run_id": dag_run_id, "conf": payload},
+        auth=airflow_auth,
+    )
+    if not resp.ok:
+        raise HTTPException(status_code=resp.status_code, detail=resp.text)
+    return resp.json()
 
 @app.post("/run-sum/{dag_run_id}")
 def get_result(dag_run_id: str):
     deadline = time.time() + POLL_TIMEOUT
     state = None
-    AIRFLOW_AUTH = _load_credentials()
+    airflow_auth = _load_credentials()
     while time.time() < deadline:
-        response = requests.get(f"{AIRFLOW_API}/dags/{DAG_ID}/dagRuns/{dag_run_id}", auth=AIRFLOW_AUTH)
+        response = requests.get(f"{AIRFLOW_API}/dags/{DAG_ID}/dagRuns/{dag_run_id}", auth=airflow_auth)
         if not response.ok:
             raise HTTPException(status_code=response.status_code,detail=response.text)
         state = response.json().get("state")
@@ -48,7 +61,7 @@ def get_result(dag_run_id: str):
     
     # fetch XCom for the task that returns the total (task id: load)
     task_id = "load"
-    xcom_response = requests(f"{AIRFLOW_API}/dags/{DAG_ID}/dagRuns/{dag_run_id}/taskInstances/{task_id}/xcomEntries",auth=AIRFLOW_AUTH)
+    xcom_response = requests(f"{AIRFLOW_API}/dags/{DAG_ID}/dagRuns/{dag_run_id}/taskInstances/{task_id}/xcomEntries",auth=airflow_auth)
     if not xcom_response.ok:
         raise HTTPException(status_code=xcom_response.status_code, detail=xcom_response.text)
     xcoms = xcom_response.json().get("xcom_entries",[])
